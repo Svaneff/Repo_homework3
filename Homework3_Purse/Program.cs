09769c76d285528eff0500bea377b30f772dd614
Yurii Krutsko <yuri.krutsko@gmail.com>
2022-04-10 23:44:28 +0300

    MoneyLib added

--- Homework3_Purse/Program.cs
+++ Homework3_Purse/Program.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessCardLib;
+using MoneyLib;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +21,16 @@ namespace Homework3_Purse
             BC.PutMoney(50000);
             BC.ShowBalance();
             //----------------------------------------
+
+            // Yurii
+            //-------------------------------------------
+            Money money = new Money();
+            money.Balance = 2000;
+            money.Id = "2-356789";
+            money.PutMoney(4500);
+            money.WithdrawMoney(300);
+            Console.WriteLine(money.Balance);
+            //-------------------------------------------
         }
     }
 }
---
+++ MoneyLib/Money.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyLib
+{
+    public class Money
+    {
+        public double Balance { get; set; }
+        public string Id { get; set; }
+
+        public Money(double balance, string id) =>
+            (Balance, Id) = (balance, id);
+
+        public Money()
+        {
+
+        }
+
+        public void WithdrawMoney(double sum)
+        {
+            Balance -= sum;
+        }
+
+        public void PutMoney(double sum)
+        {
+            Balance += sum;
+        }
+    }
+}
